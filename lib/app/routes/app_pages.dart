8805552a8f7a7e65251177de1cf46f5572c0c704
Achmad Fahreza <81659348+achreza@users.noreply.github.com>
2022-05-22 14:15:36 +0700

    add riwayat order

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -23,7 +23,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.RIWAYAT_ORDER;
+  static const INITIAL = Routes.HOME;
 
   static final routes = [
     GetPage(
